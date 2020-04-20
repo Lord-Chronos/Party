37d56a81298ff86fd7a8aa823f9471de9958eef0
Josh <j.murray567@gmail.com>
2020-04-20 13:51:46 +0100

    Step 2.1 Commented

--- src/Profile.java
+++ src/Profile.java
@@ -25,88 +25,178 @@ public class Profile {
 
     }
 
+    /**
+     * Sets last name as users input
+     *
+     * @param lastName Last name of profile
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Sets first name as users input
+     *
+     * @param firstName First name of profile
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Sets day of birth as input
+     *
+     * @param day Day of birth of profile
+     */
     public void setDay(int day) {
         this.day = day;
     }
 
+    /**
+     * Sets month of birth as input
+     *
+     * @param month Month of birth of profile
+     */
     public void setMonth(int month) {
         this.month = month;
     }
 
+    /**
+     * Sets year of birth as input
+     *
+     * @param year Year of birth of profile
+     */
     public void setYear(int year) {
         this.year = year;
     }
 
+    /**
+     * Sets email as users input
+     *
+     * @param email Email linked to profile
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Sets interests as users input
+     * @param interests Interests of the profile
+     */
     public void setInterests(String[] interests) {
         this.interests = interests;
     }
 
+    /**
+     * Sets activitiesGroups as users input
+     * @param activitiesGroups Activities and groups that the profile takes part in
+     */
     public void setActivitiesGroups(String[] activitiesGroups) {
         this.activitiesGroups = activitiesGroups;
     }
 
+    /**
+     * Gets the last name of profile
+     * @return lastName from profile
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Gets the first name of profile
+     * @return firstName from profile
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Gets the day of birth of profile
+     * @return day of birth from profile
+     */
     public int getDay() {
         return day;
     }
 
+    /**
+     * Gets the month of birth of profile
+     * @return month of birth from profile
+     */
     public int getMonth() {
         return month;
     }
 
+    /**
+     * Gets the year of birth of profile
+     * @return year of birth from profile
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     * Gets the full date of birth of profile
+     * @return Date of birth in "dd/mm/yyyy" format
+     */
     public String getDateOfBirth() {
         return (day + "/" + month + "/" + year);
     }
 
+    /**
+     * Gets the email of the profile
+     * @return email linked to profile
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Gets interests of profile
+     * @return Array of profiles interests
+     */
     public String[] getInterests() {
         return interests;
     }
 
+    /**
+     * Gets activities and groups that the profile takes part in
+     * @return Array of activities and groups
+     */
     public String[] getActivitiesGroups() {
         return activitiesGroups;
     }
 
-
-    public void addFriend(Profile p){
+    /**
+     * Adds the inputed friend to profiles friend list
+     * @param p Profile of the friend to be added
+     */
+    public void addFriend(Profile p) {
         friends.add(p);
     }
 
-    public Profile getFriend(int i){
+    /**
+     * Returns friend from profile at specified index
+     * @param i index location to get friend from
+     * @return Friends profile at input position
+     */
+    public Profile getFriend(int i) {
         return friends.get(i);
     }
 
-    public int numOfFriends(){
+    /**
+     * Gets number of friends associated to this profile
+     * @return size of friends list
+     */
+    public int numOfFriends() {
         return friends.size();
     }
 
-    public String toString(){
+    /**
+     * Converts profile to string, for debug only
+     * @return Profile info as string
+     */
+    public String toString() {
         String debug = "First Name: " + firstName + "\n";
         debug += "Last Name: " + lastName + "\n";
         debug += "Day: " + day + "\n";
@@ -114,7 +204,7 @@ public class Profile {
         debug += "Year: " + year + "\n";
         debug += "Email: " + email + "\n";
         debug += "interests: " + Arrays.toString(interests) + "\n";
-        debug += "Activities: " + Arrays.toString(activitiesGroups) + "\n" ;
+        debug += "Activities: " + Arrays.toString(activitiesGroups) + "\n";
         return debug;
     }
 }
